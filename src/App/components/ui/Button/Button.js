38d2435dc790abeb5aac33498de785bec6a471e7
Nhat Quang HO <nhquho@ad.phedre.fr>
2023-05-02 16:49:05 +0200

    css + proptype + defaultprops

--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,21 +1,49 @@
 
 import React from 'react'
 import styles from "./Button.module.css";
+import PropTypes from 'prop-types'
 
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
 const Button = (props) => {
     console.log(props)
     return (
         <div>
-            <button 
+            <button
+                style={{
+                    ...props.style,
+                    backgroundColor: props.bgColor,
+                    
+                }}
                 className={styles.MyButton}
                 onClick={(e) => {
-                    props.onClick && props.onClick("ALLO");
-                }} 
-            > 
-                {props.children} 
+                    props.onClick("ALLO");
+                    
+                }}
+            >
+                {props.children}
             </button>
         </div>
     )
+};
+
+//définir les types des props
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.any.isRequired,
+    text: PropTypes.any.isRequired,
+    bgColor: PropTypes.any.isRequired,
+    style: PropTypes.object
+};
+//définir les valeurs des props par défaut
+Button.defaultProps = {
+    children: "hello",
+    onClick: () => { },
+    text: "default",
+    bgColor:"white"
 };
 
 export default Button;
